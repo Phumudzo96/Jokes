d6752a721cf460358a949bb82b13f4409f2d1e30
Phumudzo <103152398+Phumudzo96@users.noreply.github.com>
2023-02-15 01:20:50 +0200

    Add files via upload

---
+++ jokes.py
@@ -0,0 +1,15 @@
+import random                                # import the "random" bulit-in function
+
+# Create a list full of jokes
+jokes = ["How do you measure a snake-In inches—they don't have feet",
+"What do dentists call their x-rays-Tooth pics",
+"Did you hear about the first restaurant to open on the moon-It had great food, but no atmosphere",
+"What did one ocean say to the other ocean-Nothing, it just waved",
+"Do you want to hear a construction joke-Sorry, I'm still working on it",
+"Did you hear about the fire at the circus-It was in tents!"
+"Why do ducks have feathers-To cover their butt quacks",
+"What does a nosey pepper do-It gets jalapeño busines",
+"Why did the bullet end up losing his job-He got fired",
+" What kind of shorts do clouds wear-Thunderpants"]
+
+print(random.choice(jokes))   #let the bulit-in function choose a random joke from list
